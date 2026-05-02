6a0c09538da36b6b847bbf1d27697929b312d2bf
franjim2025 <faco70@gmail.com>
2026-05-01 23:03:54 -0500

    Update del app.config

--- app.config.ts
+++ app.config.ts
@@ -3,5 +3,11 @@ import { defineConfig } from '@tanstack/start/config'
 export default defineConfig({
   server: {
     preset: 'vercel'
+  },
+  // Esto fuerza a la infraestructura a usar Vercel y no Cloudflare
+  vinxi: {
+    server: {
+      preset: 'vercel'
+    }
   }
 })
